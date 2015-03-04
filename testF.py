656c9e0ad86cd30476de65b1dd3fd2ae8e62439c
Jose <Jose@TON-Laptop>
2015-03-04 14:28:08 -0800

    Brian give me access

--- testF.py
+++ testF.py
@@ -7,4 +7,4 @@ client_s = "1EPZ1OFADQFWVKT41QE10G5C1C03QPM0BPLPXM0DOZLHS3FL"
 # Construct the client object
 client = foursquare.Foursquare(client_id=client_i, client_secret=client_s)
 
-print client.venues.stats('40a55d80f964a52020f31ee3')
+print client.venues('40a55d80f964a52020f31ee3')
