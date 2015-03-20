931e20d5a379f9669ea0492ba18619bf82f5d075
Brian Huynh <brianhuynh1@gmail.com>
2015-03-20 12:24:03 -0700

    Clean up on aisle G!

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 # BuzzSpots
-<<<<<<< HEAD
 Users will be able to see the busiest local businesses in any city with data from Foursquare
 and get information about those local businesses with Yelp's data.
-=======
-Users will be able to see the busiest local businesses in any city with data from Foursquare 
-and get information about those local businesses with Yelp's data. 
->>>>>>> origin/master
 
 # Features
 Utitlize Yelp API to provide users restaurant information.
@@ -15,7 +10,6 @@ If time persist in future, users can search for businesses popularity
 via custom search terms.
 
 # Tasks to be accomplished
-<<<<<<< HEAD
 Display locations by placing markers on a map.
 Service must be able to run on website.
 Get all files hosted on Google App Engine
@@ -25,16 +19,4 @@ Python |
 Yelp API |
 Foursquare API |
 Django |
-=======
-Retrieve restaurant data from Yelp API.
-Retrieve check-in data from Foursquares API.
-Display data in visiually appealing way for users.
-Service must be able to run on website
-
-# Tools
-Python | 
-Yelp API |
-Foursquare API | 
-Django | 
->>>>>>> origin/master
 Google App Engine
--- yelp.py
+++ yelp.py
@@ -1,7 +1,6 @@
 import rauth
 import time
 import json
-<<<<<<< HEAD
 import keys
 
 
@@ -24,33 +23,11 @@ def get_results(params):
   token = keys.YELP_TOKEN
   token_secret = keys.YELP_TOKEN_SECRET
 
-=======
-
-def get_search_parameters(lat, long):
-  # See the Yelp API for more details
-  params = {}
-  params["term"] = "restaurant"
-  params["ll"] = "{},{}".format(str(lat), str(long))
-  params["radius_filter"] = "2000"
-  params["limit"] = "10"
- 
-  return params 
-
-def get_results(params):
- 
-  # Obtain these from Yelp's manage access page
-  consumer_key = "YOUR_KEY"
-  consumer_secret = "YOUR_SECRET"
-  token = "YOUR_TOKEN"
-  token_secret = "YOUR_TOKEN_SECRET"
-   
->>>>>>> origin/master
   session = rauth.OAuth1Session(
     consumer_key=consumer_key
     , consumer_secret=consumer_secret
     , access_token=token
     , access_token_secret=token_secret)
-<<<<<<< HEAD
 
   request = session.get("http://api.yelp.com/v2/search", params=params)
 
@@ -61,29 +38,3 @@ def get_results(params):
 
 def get_yelp_resturantData(name, lat, lng):
     print get_results(get_search_parameters(name, lat, lng))
-=======
-     
-  request = session.get("http://api.yelp.com/v2/search", params=params)
-   
-  # Transforms the JSON API response into a Python dictionary
-  data = request.json()
-  session.close()
-   
-  return data
-
-def main():
-  locations = [(39.98,-82.98),(42.24,-83.61),(41.33,-89.13)]
-  api_calls = []
-  for lat, long in locations:
-    params = get_search_parameters(lat, long)
-    api_calls.append(get_results(params))
-    # Be a good internet citizen and rate-limit yourself
-    time.sleep(1.0)
-     
-  # #Do other processing
-  print api_calls
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> origin/master
